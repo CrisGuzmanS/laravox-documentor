f68b5699b0be731e919622f13eb5d39e6c06ded8
crisguzmans <cristian.guzman.contacto@gmail.com>
2022-02-13 19:27:12 +0000

    readability enhanced in DocumentorServiceProvider

--- src/DocumentorServiceProvider.php
+++ src/DocumentorServiceProvider.php
@@ -6,20 +6,50 @@ use Illuminate\Support\ServiceProvider;
 
 class DocumentorServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/config/config.php',
-            'documentor'
+            $this->packageConfigPath(),
+            $this->key()
         );
     }
 
-    public function boot()
+    public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/config/config.php' => config_path('documentor.php'),
+                $this->packageConfigPath() => $this->appConfigPath(),
             ], 'config');
         }
+    }
+
+    /**
+     * Path where the config file is placed inside the package
+     * 
+     * @return string 
+     */
+    private function packageConfigPath(): string
+    {
+        return __DIR__ . '/config/config.php';
+    }
+
+    /**
+     * Path where the config file will be placed in the app
+     * 
+     */
+    private function appConfigPath(): string
+    {
+        return config_path(
+            $this->key() . '.php'
+        );
+    }
+
+    /**
+     * keyname of the config file
+     * 
+     */
+    public function key()
+    {
+        return 'documentor';
     }
 }
